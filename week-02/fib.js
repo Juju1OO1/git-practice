90362dc49dd562f9666036afaf3481fc7f30a95c
李芷勻 <61776077+Juju1OO1@users.noreply.github.com>
2024-10-01 10:24:51 +0800

    Fix : fib.js fix

--- week-02/fib.js
+++ week-02/fib.js
@@ -1,16 +1,16 @@
 function fib(n){
-    //以三個連續的 fibonacci 數為一組： fLast,fPre,fNext
+    // s以三個連續的 fibonacci 數為一組： fLast,fPre,fNext
     let fNext=0;
     let fLast=0;
     let fPre=1;
     
     if(n==0){
-    //第0個 fibonacci 數為 0 ，直接輸出 0
+    // 第0個 fibonacci 數為 0 ，直接輸出 0
         return(fLast);
     }else{
-    //將 fLast,fPre 相加求出下一個 fibonacci 數存入 fNext
-    //再將 fLast,fPre 各往後移一個 fibonacci 數
-    //以迴圈執行到找到第 n 個 fibonacci 數 fPre
+    // 將 fLast,fPre 相加求出下一個 fibonacci 數存入 fNext
+    // 再將 fLast,fPre 各往後移一個 fibonacci 數
+    // 以迴圈執行到找到第 n 個 fibonacci 數 fPre
         for(let i=1;i<n;i++){
             fNext = fLast+fPre;
             fLast = fPre;
